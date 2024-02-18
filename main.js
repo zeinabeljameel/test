3440cfe9ad3cdd9647a3d9968c73f70012f79549
zeinabeljameel <160382093+zeinabeljameel@users.noreply.github.com>
2024-02-18 08:40:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,405 @@
+let img1 = document.querySelector('.img1')
+let img2 = document.querySelector('.img2')
+let img3 = document.querySelector('.img3')
+let img4 = document.querySelector('.img4')
+let img5 = document.querySelector('.img5')
+let img6 = document.querySelector('.img6')
+let img7 = document.querySelector('.img7')
+let img8 = document.querySelector('.img8')
+let img9 = document.querySelector('.img9')
+let img10 = document.querySelector('.img10')
+let img11 = document.querySelector('.img11')
+let img12 = document.querySelector('.img12')
+let img13 = document.querySelector('.img13')
+
+
+let next1 = document.querySelector('.next1')
+let next2 = document.querySelector('.next2')
+let next3 = document.querySelector('.next3')
+let next4 = document.querySelector('.next4')
+let next5 = document.querySelector('.next5')
+let next6 = document.querySelector('.next6')
+let next7 = document.querySelector('.next7')
+let next8 = document.querySelector('.next8')
+let next9 = document.querySelector('.next9')
+let next10 = document.querySelector('.next10')
+let next11 = document.querySelector('.next11')
+let next12 = document.querySelector('.next12')
+let next13 = document.querySelector('.next13')
+
+let before1 = document.querySelector('.before1')
+let before2 = document.querySelector('.before2')
+let before3 = document.querySelector('.before3')
+let before4 = document.querySelector('.before4')
+let before5 = document.querySelector('.before5')
+let before6 = document.querySelector('.before6')
+let before7 = document.querySelector('.before7')
+let before8 = document.querySelector('.before8')
+let before9 = document.querySelector('.before9')
+let before10 = document.querySelector('.before10')
+let before11 = document.querySelector('.before11')
+let before12 = document.querySelector('.before12')
+let before13 = document.querySelector('.before13')
+
+
+let animationNext1 = document.querySelector('.container1')
+let animationNext2 = document.querySelector('.container2')
+let animationNext3 = document.querySelector('.container3')
+let animationNext4 = document.querySelector('.container4')
+let animationNext5 = document.querySelector('.container5')
+let animationNext6 = document.querySelector('.container6')
+let animationNext7 = document.querySelector('.container7')
+let animationNext8 = document.querySelector('.container8')
+let animationNext9 = document.querySelector('.container9')
+let animationNext10 = document.querySelector('.container10')
+let animationNext11 = document.querySelector('.container11')
+let animationNext12 = document.querySelector('.container12')
+let animationNext13 = document.querySelector('.container13')
+
+let animationBefore1 = document.querySelector('.container-before1')
+let animationBefore2 = document.querySelector('.container-before2')
+let animationBefore3 = document.querySelector('.container-before3')
+let animationBefore4 = document.querySelector('.container-before4')
+let animationBefore5 = document.querySelector('.container-before5')
+let animationBefore6 = document.querySelector('.container-before6')
+let animationBefore7 = document.querySelector('.container-before7')
+let animationBefore8 = document.querySelector('.container-before8')
+let animationBefore9 = document.querySelector('.container-before9')
+let animationBefore10 = document.querySelector('.container-before10')
+let animationBefore11 = document.querySelector('.container-before11')
+let animationBefore12 = document.querySelector('.container-before12')
+let animationBefore13 = document.querySelector('.container-before13')
+
+
+
+
+
+
+
+
+next1.onclick = function(){
+    img1.style.opacity = '0'
+    setTimeout(() => {
+        img1.style.display = 'none'
+    }, 1000);
+}
+
+next2.onclick = function(){
+    img2.style.opacity = '0'
+    setTimeout(() => {
+        img2.style.display = 'none'
+    }, 1000);
+}
+
+next3.onclick = function(){
+    img3.style.opacity = '0'
+    setTimeout(() => {
+        img3.style.display = 'none'
+    }, 1000);
+}
+next4.onclick = function(){
+    img4.style.opacity = '0'
+    setTimeout(() => {
+        img4.style.display = 'none'
+    }, 1000);
+}
+next5.onclick = function(){
+    img5.style.opacity = '0'
+    setTimeout(() => {
+        img5.style.display = 'none'
+    }, 1000);
+}
+next6.onclick = function(){
+    img6.style.opacity = '0'
+    setTimeout(() => {
+        img6.style.display = 'none'
+    }, 1000);
+}
+next7.onclick = function(){
+    img7.style.opacity = '0'
+    setTimeout(() => {
+        img7.style.display = 'none'
+    }, 1000);
+}
+next8.onclick = function(){
+    img8.style.opacity = '0'
+    setTimeout(() => {
+        img8.style.display = 'none'
+    }, 1000);
+}
+next9.onclick = function(){
+    img9.style.opacity = '0'
+    setTimeout(() => {
+        img9.style.display = 'none'
+    }, 1000);
+}
+next10.onclick = function(){
+    img10.style.opacity = '0'
+    setTimeout(() => {
+        img10.style.display = 'none'
+    }, 1000);
+}
+next11.onclick = function(){
+    img11.style.opacity = '0'
+    setTimeout(() => {
+        img11.style.display = 'none'
+    }, 1000);
+}
+next12.onclick = function(){
+    img12.style.opacity = '0'
+    setTimeout(() => {
+        img12.style.display = 'none'
+    }, 1000);
+}
+
+
+
+
+before2.onclick = function(){
+    img1.style.opacity = '1'
+    img1.style.display = 'block'
+}
+before3.onclick = function(){
+    img2.style.opacity = '1'
+    img2.style.display = 'block'
+}
+before4.onclick = function(){
+    img3.style.opacity = '1'
+    img3.style.display = 'block'
+}
+before5.onclick = function(){
+    img4.style.opacity = '1'
+    img4.style.display = 'block'
+}
+before6.onclick = function(){
+    img5.style.opacity = '1'
+    img5.style.display = 'block'
+}
+before7.onclick = function(){
+    img6.style.opacity = '1'
+    img6.style.display = 'block'
+}
+before8.onclick = function(){
+    img7.style.opacity = '1'
+    img7.style.display = 'block'
+}
+before9.onclick = function(){
+    img8.style.opacity = '1'
+    img8.style.display = 'block'
+}
+before10.onclick = function(){
+    img9.style.opacity = '1'
+    img9.style.display = 'block'
+}
+before11.onclick = function(){
+    img10.style.opacity = '1'
+    img10.style.display = 'block'
+}
+before12.onclick = function(){
+    img11.style.opacity = '1'
+    img11.style.display = 'block'
+}
+before13.onclick = function(){
+    img12.style.opacity = '1'
+    img12.style.display = 'block'
+}
+
+
+
+
+animationNext1.onclick = function(){
+    img1.style.opacity = '0'
+    setTimeout(() => {
+        img1.style.display = 'none'
+    }, 1000);
+}
+animationNext2.onclick = function(){
+    img2.style.opacity = '0'
+    setTimeout(() => {
+        img2.style.display = 'none'
+    }, 1000);
+}
+animationNext3.onclick = function(){
+    img3.style.opacity = '0'
+    setTimeout(() => {
+        img3.style.display = 'none'
+    }, 1000);
+}
+animationNext4.onclick = function(){
+    img4.style.opacity = '0'
+    setTimeout(() => {
+        img4.style.display = 'none'
+    }, 1000);
+}
+animationNext5.onclick = function(){
+    img5.style.opacity = '0'
+    setTimeout(() => {
+        img5.style.display = 'none'
+    }, 1000);
+}
+animationNext6.onclick = function(){
+    img6.style.opacity = '0'
+    setTimeout(() => {
+        img6.style.display = 'none'
+    }, 1000);
+}
+animationNext7.onclick = function(){
+    img7.style.opacity = '0'
+    setTimeout(() => {
+        img7.style.display = 'none'
+    }, 1000);
+}
+animationNext8.onclick = function(){
+    img8.style.opacity = '0'
+    setTimeout(() => {
+        img8.style.display = 'none'
+    }, 1000);
+}
+animationNext9.onclick = function(){
+    img9.style.opacity = '0'
+    setTimeout(() => {
+        img9.style.display = 'none'
+    }, 1000);
+}
+animationNext10.onclick = function(){
+    img10.style.opacity = '0'
+    setTimeout(() => {
+        img10.style.display = 'none'
+    }, 1000);
+}
+animationNext11.onclick = function(){
+    img11.style.opacity = '0'
+    setTimeout(() => {
+        img11.style.display = 'none'
+    }, 1000);
+}
+animationNext12.onclick = function(){
+    img12.style.opacity = '0'
+    setTimeout(() => {
+        img12.style.display = 'none'
+    }, 1000);
+}
+
+
+
+
+animationBefore2.onclick = function(){
+    img2.style.opacity = '0'
+    setTimeout(() => {
+        img2.style.display = 'none'
+    }, 1000);
+}
+animationBefore2.onclick = function(){
+    img1.style.opacity = '1'
+    img1.style.display = 'block'
+}
+animationBefore3.onclick = function(){
+    img3.style.opacity = '0'
+    setTimeout(() => {
+        img3.style.display = 'none'
+    }, 1000);
+}
+animationBefore3.onclick = function(){
+    img2.style.opacity = '1'
+    img2.style.display = 'block'
+}
+animationBefore4.onclick = function(){
+    img4.style.opacity = '0'
+    setTimeout(() => {
+        img4.style.display = 'none'
+    }, 1000);
+}
+animationBefore4.onclick = function(){
+    img3.style.opacity = '1'
+    img3.style.display = 'block'
+}
+animationBefore5.onclick = function(){
+    img5.style.opacity = '0'
+    setTimeout(() => {
+        img5.style.display = 'none'
+    }, 1000);
+}
+animationBefore5.onclick = function(){
+    img4.style.opacity = '1'
+    img4.style.display = 'block'
+}
+animationBefore6.onclick = function(){
+    img6.style.opacity = '0'
+    setTimeout(() => {
+        img6.style.display = 'none'
+    }, 1000);
+}
+animationBefore6.onclick = function(){
+    img5.style.opacity = '1'
+    img5.style.display = 'block'
+}
+animationBefore7.onclick = function(){
+    img7.style.opacity = '0'
+    setTimeout(() => {
+        img7.style.display = 'none'
+    }, 1000);
+}
+animationBefore7.onclick = function(){
+    img6.style.opacity = '1'
+    img6.style.display = 'block'
+}
+animationBefore8.onclick = function(){
+    img8.style.opacity = '0'
+    setTimeout(() => {
+        img8.style.display = 'none'
+    }, 1000);
+}
+animationBefore8.onclick = function(){
+    img7.style.opacity = '1'
+    img7.style.display = 'block'
+}
+animationBefore9.onclick = function(){
+    img9.style.opacity = '0'
+    setTimeout(() => {
+        img9.style.display = 'none'
+    }, 1000);
+}
+animationBefore9.onclick = function(){
+    img8.style.opacity = '1'
+    img8.style.display = 'block'
+}
+animationBefore10.onclick = function(){
+    img10.style.opacity = '0'
+    setTimeout(() => {
+        img10.style.display = 'none'
+    }, 1000);
+}
+animationBefore10.onclick = function(){
+    img9.style.opacity = '1'
+    img9.style.display = 'block'
+}
+animationBefore11.onclick = function(){
+    img11.style.opacity = '0'
+    setTimeout(() => {
+        img11.style.display = 'none'
+    }, 1000);
+}
+animationBefore11.onclick = function(){
+    img10.style.opacity = '1'
+    img10.style.display = 'block'
+}
+animationBefore12.onclick = function(){
+    img12.style.opacity = '0'
+    setTimeout(() => {
+        img12.style.display = 'none'
+    }, 1000);
+}
+animationBefore12.onclick = function(){
+    img11.style.opacity = '1'
+    img11.style.display = 'block'
+}
+animationBefore13.onclick = function(){
+    img13.style.opacity = '0'
+    setTimeout(() => {
+        img13.style.display = 'none'
+    }, 1000);
+}
+animationBefore13.onclick = function(){
+    img12.style.opacity = '1'
+    img12.style.display = 'block'
+}
